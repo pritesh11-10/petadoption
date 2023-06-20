477e00eaab09724cb04cb8c21ae51996f51a335c
pritesh11-10 <61231518+pritesh11-10@users.noreply.github.com>
2023-06-20 11:36:10 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import {createRoot } from "react-dom/client"
+import { useState } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
+import SearchParams from "./SearchParams";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+//npm run dev
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  const adoptedPet = useState(null);
+  return (
+    <div className="p-0 m-0" style={{
+      background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+    }}>
+      <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+      <AdoptedPetContext.Provider value={adoptedPet}>
+      <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500">
+        <Link className="text-6xl text-white hover:text-gray-200" to="/">Adopt Me!</Link>
+      </header>
+    <Routes>
+      <Route path="/details/:id" element={<Details />} />
+      <Route path="/" element={<SearchParams />} />
+    </Routes>
+    </AdoptedPetContext.Provider>
+    </QueryClientProvider>
+  </BrowserRouter>;
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
